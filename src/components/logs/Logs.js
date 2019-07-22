3e0c8695296e8b1664fc802d0c5513e6f385db8f
Алексей Бельченков <u608110@gmail.com>
2019-07-22 12:49:47 +0400

    LogItem Component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import LogItem from "./LogItem";
+
 const Logs = () => {
     const [logs, setLogs] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -9,7 +11,6 @@ const Logs = () => {
 
         const res = await fetch('/logs');
         const data = await res.json();
-        console.log(data);
 
         setLogs(data);
         setLoading(false);
@@ -26,13 +27,13 @@ const Logs = () => {
     }
 
     return (
-        <ul className="collection-with-header">
+        <ul className="collection with-header">
             <li className="collection-header">
                 <h4 className="center">System Logs</h4>
             </li>
             {!loading && logs.length === 0 ? (
                 <p className="center">No logs to show...</p>
-                ) : logs.map(log => <li>{log.message}</li>)
+                ) : logs.map(log => <LogItem log={log} key={log.id} />)
             }
         </ul>
     );
